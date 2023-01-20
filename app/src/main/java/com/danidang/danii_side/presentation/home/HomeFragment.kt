01713da2dbf50308750ac41f60b9ea8b50cb68bc
Daeun <eldms0406@naver.com>
2023-01-20 18:28:58 +0900

    [feat] fragment 구현(임시 디자인)

---
+++ app/src/main/java/com/danidang/danii_side/presentation/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.danidang.danii_side.presentation.home
+
+import android.os.Bundle
+import com.danidang.danii_side.R
+import com.danidang.danii_side.databinding.FragmentHomeBinding
+import com.danidang.danii_side.util.binding.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/danidang/danii_side/presentation/home/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.danidang.danii_side.presentation.home
+
+import com.danidang.danii_side.R
+import com.danidang.danii_side.databinding.FragmentSearchBinding
+import com.danidang.danii_side.util.binding.BindingFragment
+
+class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search)
