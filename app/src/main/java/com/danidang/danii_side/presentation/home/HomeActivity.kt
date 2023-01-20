bc544b9f1540be743eb7995a924116ab4126c5a6
Daeun <eldms0406@naver.com>
2023-01-20 18:29:11 +0900

    [feat] 바텀 네비게이션 구현

--- app/src/main/java/com/danidang/danii_side/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/danidang/danii_side/presentation/home/HomeActivity.kt
@@ -1,12 +1,31 @@
 package com.danidang.danii_side.presentation.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.danidang.danii_side.R
+import com.danidang.danii_side.databinding.ActivityHomeBinding
+import com.danidang.danii_side.util.binding.BindingActivity
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        changeFragment(HomeFragment())
+        binding.bnvHome.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.menu_home -> changeFragment(HomeFragment())
+                    R.id.menu_gallery -> changeFragment(GalleryFragment())
+                    R.id.menu_search -> changeFragment(SearchFragment())
+                }
+                true
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.home_container, fragment)
+            .commit()
     }
 }
