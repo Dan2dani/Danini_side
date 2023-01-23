9031f3f4112626644a63822eb68c8a4e822d739f
Daeun <eldms0406@naver.com>
2023-01-23 22:34:06 +0900

    [feat] 멀티뷰 구현

--- app/src/main/java/com/danidang/danii_side/presentation/github/RepoAdapter.kt
+++ app/src/main/java/com/danidang/danii_side/presentation/github/RepoAdapter.kt
@@ -5,8 +5,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.danidang.danii_side.databinding.ItemRepoBinding
+import com.danidang.danii_side.databinding.LayoutGithubHeaderBinding
 
-class RepoAdapter(context: Context) : RecyclerView.Adapter<RepoAdapter.RepoViewHolder>() {
+class RepoAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val inflater by lazy { LayoutInflater.from(context) }
     private var repoList: List<Repo> = emptyList()
 
@@ -20,19 +21,45 @@ class RepoAdapter(context: Context) : RecyclerView.Adapter<RepoAdapter.RepoViewH
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
-        val binding = ItemRepoBinding.inflate(inflater, parent, false)
-        return RepoViewHolder(binding)
+    class HeaderViewHolder(
+        private val binding: LayoutGithubHeaderBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            HEADER_TYPE -> {
+                val binding = LayoutGithubHeaderBinding.inflate(inflater, parent, false)
+                HeaderViewHolder(binding)
+            }
+            else -> {
+                val binding = ItemRepoBinding.inflate(inflater, parent, false)
+                RepoViewHolder(binding)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
-        holder.onBind(repoList[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is RepoViewHolder -> holder.onBind(repoList[position - 1])
+        }
     }
 
-    override fun getItemCount() = repoList.size
+    override fun getItemCount() = repoList.size + 1
 
-    fun setRepoList(repoList: List<Repo>){
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> HEADER_TYPE
+            else -> REPO_TYPE
+        }
+    }
+
+    fun setRepoList(repoList: List<Repo>) {
         this.repoList = repoList.toList()
         notifyDataSetChanged()
+    }
+
+    companion object {
+        const val HEADER_TYPE = 1
+        const val REPO_TYPE = 2
     }
 }
