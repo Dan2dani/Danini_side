26cf1969b40dac00e13b358b91457164be23c0cd
Daeun <eldms0406@naver.com>
2023-02-02 22:43:04 +0900

    [feat] Tablayout을 ViewPager2에 연결해 indicator 구현

--- app/src/main/java/com/danidang/danii_side/presentation/gallery/GalleryFragment.kt
+++ app/src/main/java/com/danidang/danii_side/presentation/gallery/GalleryFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.danidang.danii_side.R
 import com.danidang.danii_side.databinding.FragmentGalleryBinding
 import com.danidang.danii_side.util.binding.BindingFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragment_gallery) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -15,5 +16,6 @@ class GalleryFragment : BindingFragment<FragmentGalleryBinding>(R.layout.fragmen
     private fun initLayout() {
         val adapter = GalleryAdapter(requireContext())
         binding.vpGalleryImage.adapter = adapter
+        TabLayoutMediator(binding.indicator, binding.vpGalleryImage) { _, _ -> }.attach()
     }
 }
